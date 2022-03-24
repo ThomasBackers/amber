617927a4defe06bd97e2045a2f50cc7091785cd7
ThomasBackers <thomasbackers@gmail.com>
2022-03-24 11:49:01 +0100

    theme toggle switch done

--- vue/src/components/ThemeToggleSwitch.vue
+++ vue/src/components/ThemeToggleSwitch.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
-const switchTheme = (): void => {}
+import { ref, Ref } from 'vue'
+
+const root: HTMLElement = document.documentElement
+const slider: Ref<HTMLElement | null> = ref(null)
+
+const switchTheme = (): void => {
+  if (root.classList.contains('dark')) {
+    root.classList.remove('dark')
+    if (slider.value) slider.value.style.transform = 'translateX(0px)'
+    localStorage.setItem('theme', 'light')
+  } else {
+    root.classList.add('dark')
+    if (slider.value) slider.value.style.transform = 'translateX(20px)'
+    localStorage.setItem('theme', 'dark')
+  }
+}
 </script>
 
 <template>
   <div
     aria-label="theme toggle switch"
     @click="switchTheme"
-    class=""
+    class="border-2 border-gray-200 rounded-2xl w-12 flex p-1 cursor-pointer"
   >
     <button
-      aria-label="slider"
-      class=""
+      ref="slider"
+      class="bg-gray-200 w-4 h-4 rounded-2xl transition-transform"
+      :class="root.classList.contains('dark') ? 'translate-x-5' : 'translate-x-0'"
     ></button>
   </div>
 </template>
-
-<style>
-@keyframes themeToggleSwitch {
-  0% {}
-  100% {}
-}
-</style>
--- vue/src/layouts/GuestLayout.vue
+++ vue/src/layouts/GuestLayout.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import ThemeToggleSwitch from '../components/ThemeToggleSwitch.vue'
+</script>
+
 <template>
   <div class="min-h-screen dark:bg-black">
+    <ThemeToggleSwitch />
     <router-view></router-view>
   </div>
 </template>
